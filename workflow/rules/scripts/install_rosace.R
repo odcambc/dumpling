1324764bdd814f4a6b85be13d7db3b064d3da378
Chris <17653365+odcambc@users.noreply.github.com>
2025-03-28 12:27:05 -0500

    handle r envs better (maybe?) and include library checks

--- workflow/rules/scripts/install_rosace.R
+++ workflow/rules/scripts/install_rosace.R
@@ -11,27 +11,59 @@ sink(con, append = FALSE, split = FALSE, type = "message")
 sink(con, append = FALSE, split = FALSE, type = "output")
 
 
+# If user-managed rosace install is used, check it
+if (snakemake@config[["rosace_local"]]) {
+  message("Checking local R environment for Rosace.")
 
-# Install renv next
-install.packages("renv", repos = c("https://cloud.r-project.org"))
+  # Check if cmdstanr available
+  if (require(cmdstanr)) {
+    message("Local cmdstanr found.")
+  } else {
+    stop("cmdstanr not installed. Please check installation.")
+  }
 
-library("renv")
+  # Check cmdstanr toolchain
+  tryCatch(
+    {
+      install_cmdstan(overwrite = FALSE)
+    },
+    warning = function(w) {
+      message("cmdstan already installed. Not reinstalling.")
+    }
+  )
 
-renv::restore()
+  # Check Rosace install
+  if (require(rosace)) {
+    message("Local rosace found.")
+  } else {
+    stop("Rosace not available in local R environment. Please check installation.")
+  }
 
-library("cmdstanr")
+  # If local install is not used, install rosace into conda env through Renv
+} else {
+  # Install renv
+  install.packages("renv", repos = c("https://cloud.r-project.org"))
+
+  library("renv")
+
+  renv::restore()
+
+  library("cmdstanr")
 
 
-# Check toolchain before intstalling cmdstan
-check_cmdstan_toolchain()
+  # Check toolchain before intstalling cmdstan
+  check_cmdstan_toolchain()
 
-# Install cmdstan, catch warning if already installed
-tryCatch({
-  install_cmdstan(overwrite = FALSE)
-}, warning = function(w) {
-  message("cmdstan already installed. Not reinstalling.")
-})
-
+  # Install cmdstan, catch warning if already installed
+  tryCatch(
+    {
+      install_cmdstan(overwrite = FALSE)
+    },
+    warning = function(w) {
+      message("cmdstan already installed. Not reinstalling.")
+    }
+  )
+}
 # Write to output file upon successful installation
 
 con <- file(output_file, open = "wt")
